d881224b5ec3762eb565f607cc6568a8d67d3f8a
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-07-30 12:10:08 +0800

    Update configs.tsx

--- react/src/contexts/configs.tsx
+++ react/src/contexts/configs.tsx
@@ -76,7 +76,7 @@ export const ConfigsProvider = ({
 
     // 如果文本模型或工具模型为空，则显示登录对话框
     if (llmModels.length === 0 || toolList.length === 0) {
-      setShowLoginDialog(true)
+      setShowLoginDialog(false)
     }
   }, [
     modelList,
